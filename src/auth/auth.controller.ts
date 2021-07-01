1803561fb0e6c463e60fa534b323b6e653738c8f
Leonid Tankilevich <leonid@republic.co>
2021-07-02 00:49:40 +0300

    Auth and jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,36 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+  Req,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
+  signUp(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@Req() req) {
+    console.log(req);
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,18 +1,53 @@
-import { Task } from 'src/tasks/task.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository, EntityRepository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
 import { User } from './user.entity';
-
+import * as bcrypt from 'bcrypt';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
     const user = new User();
 
     user.username = username;
-    user.password = password;
+    user.password = await this.hashPassword(password, salt);
 
-    await user.save();
+    user.salt = salt;
+
+    try {
+      await user.save();
+    } catch (err) {
+      console.log(err);
+
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exist');
+      }
+
+      throw new InternalServerErrorException();
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
